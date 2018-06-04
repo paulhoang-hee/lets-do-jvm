8c4d8fea08d5b9ab2b331c71118f640a2baf541f
Paul Hoang <paul.hoang@hee.nhs.uk>
2018-06-04 10:40:18 +0100

    fixed all the shit yo

--- src/main/java/com/paulhoang/Application.java
+++ src/main/java/com/paulhoang/Application.java
@@ -1,10 +1,34 @@
 package com.paulhoang;
 
+import com.paulhoang.service.MyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class Application {
+
+  @Autowired
+  private MyService myService;
 
   public static void main(String[] args) {
 
     System.out.println("BLAH BLAH");
-
+    SpringApplication.run(Application.class, args);
   }
+
+  @RequestMapping(value = "/blah")
+  public String blah() {
+    return "blah";
+  }
+
+
+  @RequestMapping(value = "/very-heavy-compute")
+  public String veryHeavyCompute() {
+    return myService.someVeryComputeIntensiveMethod().toString();
+  }
+
 }
---
+++ src/main/java/com/paulhoang/service/MyService.java
@@ -0,0 +1,37 @@
+package com.paulhoang.service;
+
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.Queue;
+import java.util.Random;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+@Service
+public class MyService {
+
+  private static final int LOOP_TIMES = 5_000_000;
+  private Queue<Long> store;
+
+  @PostConstruct
+  public void init() {
+    store = new ConcurrentLinkedQueue<>();
+    generateRandomNumbers();
+  }
+
+  public Long someVeryComputeIntensiveMethod() {
+    generateRandomNumbers();
+    Long sum = new Long(0);
+    for (Long aLong : store) {
+      sum += aLong;
+    }
+    return sum;
+  }
+
+  private void generateRandomNumbers() {
+    Random r = new Random();
+    for (int i = 0; i < LOOP_TIMES; i++) {
+      store.add(r.nextLong());
+    }
+  }
+}
